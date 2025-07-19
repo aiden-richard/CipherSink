0d4f0ee78205d476e60416070a9347bc65ca6e90
aiden-richard <richardaiden2006@gmail.com>
2025-07-18 18:37:46 -0700

    Renamed Coordinate class to Coordinates and added comments

---
+++ CipherSink/Models/Coordinates.cs
@@ -0,0 +1,37 @@
+﻿namespace CipherSink.Models;
+
+/// <summary>
+/// This class represents a pair of coordinates (X, Y).
+/// </summary>
+internal class Coordinates
+{
+    /// <summary>
+    /// The X coordinate.
+    /// </summary>
+    public int X { get; set; }
+
+    /// <summary>
+    /// The Y coordinate.
+    /// </summary>
+    public int Y { get; set; }
+
+    /// <summary>
+    /// Conststructor to initialize the coordinates with given values.
+    /// </summary>
+    /// <param name="x">The x value to be set</param>
+    /// <param name="y">The y value to be set</param>
+    public Coordinates(int x, int y)
+    {
+        X = x;
+        Y = y;
+    }
+
+    /// <summary>
+    /// Provides a string representation of the coordinates in the format "(X, Y)".
+    /// </summary>
+    /// <returns>A string representation of the coordinates in the the format (X, Y)</returns>
+    public override string ToString()
+    {
+        return $"({X}, {Y})";
+    }
+}
