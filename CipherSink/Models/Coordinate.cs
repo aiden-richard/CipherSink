b024f22374848940164e0eb08d8936acc7e0832e
aiden-richard <richardaiden2006@gmail.com>
2025-07-18 18:28:54 -0700

    Add Coordinate class. Add Models folder for models.

---
+++ CipherSink/Models/Coordinate.cs
@@ -0,0 +1,16 @@
+﻿namespace CipherSink.Models;
+
+internal class Coordinate
+{
+    public int X { get; set; }
+    public int Y { get; set; }
+    public Coordinate(int x, int y)
+    {
+        X = x;
+        Y = y;
+    }
+    public override string ToString()
+    {
+        return $"({X}, {Y})";
+    }
+}
