e34e71eaaed9dea4cad679bfe538e30b6a85d662
aiden-richard <richardaiden2006@gmail.com>
2025-07-20 10:41:41 -0700

    Add OccupationType enum to represent Cell states. It has three values so far: Empty, Hit, and Miss

---
+++ CipherSink/Models/OccupationType.cs
@@ -0,0 +1,22 @@
+﻿// use Description attribute on enum values, if we need to print
+// the grid, these will be used. Might be useful for debugging
+using System.ComponentModel;
+
+namespace CipherSink.Models;
+
+/// <summary>
+/// Enumeration representing the occupation type of a cell in the grid.
+/// For now it only has three values: Empty, Hit, and Miss. Once Ships are implemented,
+/// we can add more values to this enum.
+/// </summary>
+public enum  OccupationType
+{
+    [Description("o")]
+    Empty,
+
+    [Description("X")]
+    Hit,
+    
+    [Description("M")]
+    Miss
+}
