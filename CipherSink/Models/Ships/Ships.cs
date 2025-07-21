3ca4509ef24dbb7c008716bd2b8c29c280045e7a
aiden-richard <richardaiden2006@gmail.com>
2025-07-21 15:30:48 -0700

    Add ship classes: Carrier, Battleship, Cruiser, Submarine, Destroyer
    Implemented five new ship types inheriting from the Ship base class. Each class is initialized with specific name, size, and occupation type attributes.

---
+++ CipherSink/Models/Ships/Ships.cs
@@ -0,0 +1,26 @@
+﻿namespace CipherSink.Models.Ships;
+
+internal class Carrier : Ship
+{
+    public Carrier() : base("Carrier", 5, OccupationType.Carrier) { }
+}
+
+internal class Battleship : Ship
+{
+    public Battleship() : base("Battleship", 4, OccupationType.Battleship) { }
+}
+
+internal class Cruiser : Ship
+{
+    public Cruiser() : base("Cruiser", 3, OccupationType.Cruiser) { }
+}
+
+internal class Submarine : Ship
+{
+    public Submarine() : base("Submarine", 3, OccupationType.Submarine) { }
+}
+
+internal class Destroyer : Ship
+{
+    public Destroyer() : base("Destroyer", 2, OccupationType.Destroyer) { }
+}
