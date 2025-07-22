959c54de94859b39440967c5a74a566393844c25
aiden-richard <richardaiden2006@gmail.com>
2025-07-22 10:59:41 -0700

    Remove unused libraries from forms and change namespace to be non-bracket style to be consistent accross project

--- CipherSink/Forms/Account.cs
+++ CipherSink/Forms/Account.cs
@@ -1,30 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Drawing.Drawing2D;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
+﻿using System.Drawing.Drawing2D;
 
-namespace CipherSink
+namespace CipherSink;
+
+public partial class Account : Form
 {
-    public partial class Account : Form
+    public Account()
     {
-        public Account()
-        {
-            InitializeComponent();
-        }
+        InitializeComponent();
+    }
 
-        protected override void OnPaintBackground(PaintEventArgs e)
+    protected override void OnPaintBackground(PaintEventArgs e)
+    {
+        // Draw a vertical gradient from yellow (top) to blue (bottom)
+        using (var brush = new LinearGradientBrush(this.ClientRectangle, Color.LightGoldenrodYellow, Color.RoyalBlue, LinearGradientMode.Vertical))
         {
-            // Draw a vertical gradient from yellow (top) to blue (bottom)
-            using (var brush = new LinearGradientBrush(this.ClientRectangle, Color.LightGoldenrodYellow, Color.RoyalBlue, LinearGradientMode.Vertical))
-            {
-                e.Graphics.FillRectangle(brush, this.ClientRectangle);
-            }
+            e.Graphics.FillRectangle(brush, this.ClientRectangle);
         }
     }
 }
--- CipherSink/Forms/Gameboard.cs
+++ CipherSink/Forms/Gameboard.cs
@@ -1,30 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Drawing.Drawing2D;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
+﻿using System.Drawing.Drawing2D;
 
-namespace CipherSink
+namespace CipherSink;
+
+public partial class Gameboard : Form
 {
-    public partial class Gameboard : Form
+    public Gameboard()
     {
-        public Gameboard()
-        {
-            InitializeComponent();
-        }
+        InitializeComponent();
+    }
 
-        protected override void OnPaintBackground(PaintEventArgs e)
+    protected override void OnPaintBackground(PaintEventArgs e)
+    {
+        // Draw a vertical gradient from yellow (top) to blue (bottom)
+        using (var brush = new LinearGradientBrush(this.ClientRectangle, Color.LightBlue, Color.RoyalBlue, LinearGradientMode.Vertical))
         {
-            // Draw a vertical gradient from yellow (top) to blue (bottom)
-            using (var brush = new LinearGradientBrush(this.ClientRectangle, Color.LightBlue, Color.RoyalBlue, LinearGradientMode.Vertical))
-            {
-                e.Graphics.FillRectangle(brush, this.ClientRectangle);
-            }
+            e.Graphics.FillRectangle(brush, this.ClientRectangle);
         }
     }
 }
