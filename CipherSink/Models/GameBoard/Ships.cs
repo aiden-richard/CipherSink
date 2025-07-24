a06a818389dcb0bf1ef629e0a6a8948e67a909f9
aiden-richard <richardaiden2006@gmail.com>
2025-07-24 15:31:17 -0700

    Move all ships logic to one file and move that file to be in the GameBoard folder. Remove the now unecessary using statement from GameBoard class.

--- CipherSink/Models/GameBoard/Ships.cs
+++ CipherSink/Models/GameBoard/Ships.cs
@@ -1,6 +1,4 @@
-﻿using CipherSink.Models.GameBoard;
-
-namespace CipherSink.Models.Ships;
+﻿namespace CipherSink.Models.GameBoard;
 
 /// <summary>
 /// This class represents a ship on the game board.
@@ -86,4 +84,30 @@ internal abstract class Ship
 
         Positions = positions;
     }
+}
+
+
+internal class Carrier : Ship
+{
+    public Carrier() : base("Carrier", 5, OccupationType.Carrier) { }
+}
+
+internal class Battleship : Ship
+{
+    public Battleship() : base("Battleship", 4, OccupationType.Battleship) { }
+}
+
+internal class Cruiser : Ship
+{
+    public Cruiser() : base("Cruiser", 3, OccupationType.Cruiser) { }
+}
+
+internal class Submarine : Ship
+{
+    public Submarine() : base("Submarine", 3, OccupationType.Submarine) { }
+}
+
+internal class Destroyer : Ship
+{
+    public Destroyer() : base("Destroyer", 2, OccupationType.Destroyer) { }
 }
