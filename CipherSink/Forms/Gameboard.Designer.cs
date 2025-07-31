38cb4aca97c7088bd28db8d854df197fb3171d58
Alex Esson <196983260+Ghostshadow1222@users.noreply.github.com>
2025-07-30 20:10:38 -0700

    Begining work to allow for grid replacement. (#47)
    * Begining work to allow for grid replacement.
    
    * Add layout panels to Gameboard form
    
    This commit introduces two new `TableLayoutPanel` controls, `EnemyPlayerTlp` and `ActivePlayerTlp`, to the `Gameboard` form. Each panel is configured with 10 columns and 10 rows, with fixed sizes for consistent layout. The positions of `TurnTrackerTbx` and `label1` have been updated to fit the new design. Additionally, the client size of the form has been increased to accommodate the new controls, which are now included in the form's controls collection.
    
    * Update UI layout and add grid interaction functionality
    
    Adjusted positions of UI elements and increased grid sizes for both player tables. Added methods to initialize and set up grid layouts, including clickable cells that change color on interaction.
    
    ---------
    
    Co-authored-by: Alex Esson <Ghostshadow1222@users.noreply.github.com>

--- CipherSink/Forms/Gameboard.Designer.cs
+++ CipherSink/Forms/Gameboard.Designer.cs
@@ -28,29 +28,15 @@
         /// </summary>
         private void InitializeComponent()
         {
-            panel1 = new Panel();
-            panel2 = new Panel();
             TurnTrackerTbx = new TextBox();
             label1 = new Label();
+            EnemyPlayerTlp = new TableLayoutPanel();
+            ActivePlayerTlp = new TableLayoutPanel();
             SuspendLayout();
-            // 
-            // panel1
-            // 
-            panel1.Location = new Point(81, 12);
-            panel1.Name = "panel1";
-            panel1.Size = new Size(302, 272);
-            panel1.TabIndex = 0;
-            // 
-            // panel2
-            // 
-            panel2.Location = new Point(81, 404);
-            panel2.Name = "panel2";
-            panel2.Size = new Size(302, 270);
-            panel2.TabIndex = 1;
             // 
             // TurnTrackerTbx
             // 
-            TurnTrackerTbx.Location = new Point(447, 338);
+            TurnTrackerTbx.Location = new Point(568, 548);
             TurnTrackerTbx.Name = "TurnTrackerTbx";
             TurnTrackerTbx.ReadOnly = true;
             TurnTrackerTbx.Size = new Size(100, 23);
@@ -63,22 +49,84 @@
             label1.BackColor = Color.Transparent;
             label1.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
             label1.ForeColor = Color.FromArgb(128, 255, 255);
-            label1.Location = new Point(478, 314);
+            label1.Location = new Point(595, 524);
             label1.Name = "label1";
             label1.Size = new Size(45, 21);
             label1.TabIndex = 3;
             label1.Text = "Turn:";
+            // 
+            // EnemyPlayerTlp
+            // 
+            EnemyPlayerTlp.ColumnCount = 11;
+            EnemyPlayerTlp.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 50F));
+            EnemyPlayerTlp.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 50F));
+            EnemyPlayerTlp.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 50F));
+            EnemyPlayerTlp.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 50F));
+            EnemyPlayerTlp.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 50F));
+            EnemyPlayerTlp.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 50F));
+            EnemyPlayerTlp.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 50F));
+            EnemyPlayerTlp.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 50F));
+            EnemyPlayerTlp.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 50F));
+            EnemyPlayerTlp.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 50F));
+            EnemyPlayerTlp.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 50F));
+            EnemyPlayerTlp.Location = new Point(12, 12);
+            EnemyPlayerTlp.Name = "EnemyPlayerTlp";
+            EnemyPlayerTlp.RowCount = 11;
+            EnemyPlayerTlp.RowStyles.Add(new RowStyle(SizeType.Absolute, 50F));
+            EnemyPlayerTlp.RowStyles.Add(new RowStyle(SizeType.Absolute, 50F));
+            EnemyPlayerTlp.RowStyles.Add(new RowStyle(SizeType.Absolute, 50F));
+            EnemyPlayerTlp.RowStyles.Add(new RowStyle(SizeType.Absolute, 50F));
+            EnemyPlayerTlp.RowStyles.Add(new RowStyle(SizeType.Absolute, 50F));
+            EnemyPlayerTlp.RowStyles.Add(new RowStyle(SizeType.Absolute, 50F));
+            EnemyPlayerTlp.RowStyles.Add(new RowStyle(SizeType.Absolute, 50F));
+            EnemyPlayerTlp.RowStyles.Add(new RowStyle(SizeType.Absolute, 50F));
+            EnemyPlayerTlp.RowStyles.Add(new RowStyle(SizeType.Absolute, 50F));
+            EnemyPlayerTlp.RowStyles.Add(new RowStyle(SizeType.Absolute, 50F));
+            EnemyPlayerTlp.RowStyles.Add(new RowStyle(SizeType.Absolute, 50F));
+            EnemyPlayerTlp.Size = new Size(512, 512);
+            EnemyPlayerTlp.TabIndex = 4;
+            // 
+            // ActivePlayerTlp
+            // 
+            ActivePlayerTlp.ColumnCount = 11;
+            ActivePlayerTlp.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 50F));
+            ActivePlayerTlp.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 50F));
+            ActivePlayerTlp.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 50F));
+            ActivePlayerTlp.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 50F));
+            ActivePlayerTlp.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 50F));
+            ActivePlayerTlp.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 50F));
+            ActivePlayerTlp.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 50F));
+            ActivePlayerTlp.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 50F));
+            ActivePlayerTlp.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 50F));
+            ActivePlayerTlp.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 50F));
+            ActivePlayerTlp.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 50F));
+            ActivePlayerTlp.Location = new Point(12, 568);
+            ActivePlayerTlp.Name = "ActivePlayerTlp";
+            ActivePlayerTlp.RowCount = 11;
+            ActivePlayerTlp.RowStyles.Add(new RowStyle(SizeType.Absolute, 50F));
+            ActivePlayerTlp.RowStyles.Add(new RowStyle(SizeType.Absolute, 50F));
+            ActivePlayerTlp.RowStyles.Add(new RowStyle(SizeType.Absolute, 50F));
+            ActivePlayerTlp.RowStyles.Add(new RowStyle(SizeType.Absolute, 50F));
+            ActivePlayerTlp.RowStyles.Add(new RowStyle(SizeType.Absolute, 50F));
+            ActivePlayerTlp.RowStyles.Add(new RowStyle(SizeType.Absolute, 50F));
+            ActivePlayerTlp.RowStyles.Add(new RowStyle(SizeType.Absolute, 50F));
+            ActivePlayerTlp.RowStyles.Add(new RowStyle(SizeType.Absolute, 50F));
+            ActivePlayerTlp.RowStyles.Add(new RowStyle(SizeType.Absolute, 50F));
+            ActivePlayerTlp.RowStyles.Add(new RowStyle(SizeType.Absolute, 50F));
+            ActivePlayerTlp.RowStyles.Add(new RowStyle(SizeType.Absolute, 50F));
+            ActivePlayerTlp.Size = new Size(512, 512);
+            ActivePlayerTlp.TabIndex = 5;
             // 
             // Gameboard
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.DarkBlue;
-            ClientSize = new Size(599, 686);
+            ClientSize = new Size(707, 1092);
+            Controls.Add(ActivePlayerTlp);
+            Controls.Add(EnemyPlayerTlp);
             Controls.Add(label1);
             Controls.Add(TurnTrackerTbx);
-            Controls.Add(panel2);
-            Controls.Add(panel1);
             FormBorderStyle = FormBorderStyle.FixedDialog;
             MaximizeBox = false;
             Name = "Gameboard";
@@ -88,10 +136,9 @@
         }
 
         #endregion
-
-        private Panel panel1;
-        private Panel panel2;
         private TextBox TurnTrackerTbx;
         private Label label1;
+        private TableLayoutPanel EnemyPlayerTlp;
+        private TableLayoutPanel ActivePlayerTlp;
     }
 }
