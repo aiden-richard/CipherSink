522f25d0d608133a225399ecd85aed6be795f5a7
aiden-richard <richardaiden2006@gmail.com>
2025-08-13 00:17:36 -0700

    tweak whitespace in db context

--- CipherSink/Models/Database/CipherSinkContext.cs
+++ CipherSink/Models/Database/CipherSinkContext.cs
@@ -1,16 +1,11 @@
 using CipherSink.Models.Database.Entities;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.VisualBasic.ApplicationServices;
-using System.Security.Cryptography;
 
 namespace CipherSink.Models.Database;
 
 public class CipherSinkContext : DbContext
 {
-    public CipherSinkContext()
-    {
-
-    }
+    public CipherSinkContext() { }
 
     public DbSet<LocalPlayer> LocalPlayers { get; set; }
     public DbSet<RemotePlayer> RemotePlayers { get; set; }
@@ -18,18 +13,18 @@ public class CipherSinkContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder options)
     {
-    #if DEBUG
+       #if DEBUG
         // Development: Store in project folder
         string baseDirectory = AppDomain.CurrentDomain.BaseDirectory;
         string solutionDirectory = Directory.GetParent(baseDirectory)?.Parent?.Parent?.Parent?.FullName ?? baseDirectory;
         string databaseDirectory = Path.Combine(solutionDirectory, "Database");
         string databasePath = Path.Combine(databaseDirectory, "CipherSink.db");
-    #else
+       #else
         // Production: Store in user app data
         string appDataPath = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
         string appFolder = Path.Combine(appDataPath, "CipherSink");
         string databasePath = Path.Combine(appFolder, "CipherSink.db");
-    #endif
+       #endif
 
         Directory.CreateDirectory(Path.GetDirectoryName(databasePath)!);
         options.UseSqlite($"Data Source={databasePath}");
